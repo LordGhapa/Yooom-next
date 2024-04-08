5e0aa651cca7e6f60e698c7c7044fcbcf606ea33
Felipe Lacerda <99082399+LordGhapa@users.noreply.github.com>
2024-04-08 13:04:39 -0300

    🐛 Bug: try fix bug with locale hour

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -15,30 +15,39 @@ export default function Home() {
     const timeString = new Intl.DateTimeFormat("en-US", options).format(
       currentTime,
     );
-      console.log(currentTime);
-      console.log(timeString);
+    //  console.log(
+    //    new Date().toLocaleTimeString("en-US", {
+    //      hour: "2-digit",
+    //      minute: "2-digit",
+    //    }),
+    //  );
+
     return timeString;
   }
+
+  function getDate() {
+    const currentDate = new Date();
+    const options: Intl.DateTimeFormatOptions = {
+      year: "numeric",
+      month: "long",
+      day: "2-digit",
+    };
+
+    const dateString = new Intl.DateTimeFormat("pt-br", options).format(
+      currentDate,
+    );
+    return dateString;
+  }
+
+  const time = new Date().toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
   
-function getDate() {
-  const currentDate = new Date();
-  const options: Intl.DateTimeFormatOptions = {
-    year: "numeric",
-    month: "long",
-    day: "2-digit",
-  };
-
-  const dateString = new Intl.DateTimeFormat("pt-br", options).format(
-    currentDate,
-  );
-  return dateString;
-}
-
-  const time = getTime(); 
   const date = getDate();
   return (
     <section className="flex size-full flex-col gap-10 text-white">
-      <div className="bg-hero sm:h-[300px] w-full rounded-[20px] bg-cover">
+      <div className="w-full rounded-[20px] bg-hero bg-cover sm:h-[300px]">
         <div className="flex h-full flex-col justify-between max-xl:px-5 max-xl:py-8 lg:p-11">
           {/* <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-xs font-normal ">
             Upcoming Meeting at 11:30 AM
@@ -48,7 +57,7 @@ function getDate() {
           "
           >
             <h1 className="text-4xl font-extrabold lg:text-7xl ">{time}</h1>
-            <p className="text-sky-1 text-lg font-medium lg:text-2xl">{date}</p>
+            <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
           </div>
         </div>
       </div>
